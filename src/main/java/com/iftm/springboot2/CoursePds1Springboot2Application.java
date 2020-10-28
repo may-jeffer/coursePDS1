ebd5eea606753d2e9bfa6890f7eca4402009d92c
may-jeffer <maiconjefferson24@gmail.com>
2020-10-27 22:23:31 -0300

    Project created

---
+++ src/main/java/com/iftm/springboot2/CoursePds1Springboot2Application.java
@@ -0,0 +1,13 @@
+package com.iftm.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePds1Springboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePds1Springboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/springboot2/CoursePds1Springboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePds1Springboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
