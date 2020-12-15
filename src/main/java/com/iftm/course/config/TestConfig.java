f9550e9562dcf40359ba065ea5aa51b25e9e645b
may-jeffer <67930286+may-jeffer@users.noreply.github.com>
2020-12-15 18:19:10 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/iftm/course/config/TestConfig.java
+++ src/main/java/com/iftm/course/config/TestConfig.java
@@ -1,32 +1,28 @@
 package com.iftm.course.config;
 
-import java.time.Instant;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-import com.sun.tools.javac.code.Attribute.Array;
+import com.iftm.course.entities.User;
+import com.iftm.course.repositories.UserRepository;
 
-public class TestConfig {
-	
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired
-	private OrderRespository orderRepository;
-	
+
 	@Override
-	prublic void run(String... args) throws Exception {
+	public void run(String... args) throws Exception {
 		
-		User u1 = new user(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "98782379", "123456");
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-	
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T19:53:07z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-08-22T19:53:07z"), u1);
-		
-		userRepository.saveAll(Array.asList(u1, u2));
-		orderRepository.saveAll(Array.asList(o1, u2, o3));
-		
+		userRepository.saveAll(Arrays.asList(u1, u2));
 	}
 }
--- src/main/java/com/iftm/course/repositories/UserRepository.java
+++ src/main/java/com/iftm/course/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.iftm.course.repositories;
 
-public class OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.iftm.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
