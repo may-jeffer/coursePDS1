b80e0f0e0c4da30d9b8cb6d3b93aab84bd979504
may-jeffer <67930286+may-jeffer@users.noreply.github.com>
2021-01-13 22:59:50 -0300

    User insert

--- src/main/java/com/iftm/course/dto/UserDTO.java
+++ src/main/java/com/iftm/course/dto/UserDTO.java
@@ -1,8 +1,11 @@
 package com.iftm.course.dto;
 
+import java.io.Serializable;
+
 import com.iftm.course.entities.User;
 
-public class UserDTO {
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
